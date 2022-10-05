2115864dbca68eb0196537377f8ce2ac8753bb9e
Danilo Sacramento <89158678+DaniloSacramento@users.noreply.github.com>
2022-10-05 15:36:12 -0300

    começando o app

---
+++ cripto/lib/model/moedaCripto.dart
@@ -0,0 +1,13 @@
+class MoedaCripto {
+  String? icone;
+  String? nome;
+  String? sigla;
+  double? preco;
+
+  MoedaCripto({
+    this.icone,
+    this.nome,
+    this.preco,
+    this.sigla,
+  });
+}
--- cripto/lib/pages/moedas.dart
+++ cripto/lib/pages/moedas.dart
@@ -1,20 +1,31 @@
+import 'package:app/model/moedaCripto.dart';
+import 'package:app/repositories/moeda_repository.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
+
 class MoedasPage extends StatelessWidget {
   const MoedasPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final tabela = MoedasRepository.tabela;
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Cripto Moedas"),
-        
-      ),
-      body: const Center(
-        child: Text("Ola mundo"),
-      ) 
-      
-    );
+        appBar: AppBar(
+          title: const Text("Cripto Moedas"),
+        ),
+        body: ListView.separated(
+          itemBuilder: (BuildContext context, int moeda){
+            return ListTile(
+              leading: Image.asset(tabela[moeda].icone.toString()),
+              title: Text(tabela[moeda].nome.toString()),
+              trailing: Text(tabela[moeda].preco.toString()),
+            );
+          },
+          padding: const EdgeInsets.all(16) ,
+          separatorBuilder: (_,__) => const Divider(),
+          itemCount: tabela.length,
+        )
+        );
   }
 }
