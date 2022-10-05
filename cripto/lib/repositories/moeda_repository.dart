597347171c9da34a9040989ab1f89bad76c51127
Danilo Sacramento <89158678+DaniloSacramento@users.noreply.github.com>
2022-10-05 16:48:46 -0300

    imagens do projeto

--- cripto/lib/repositories/moeda_repository.dart
+++ cripto/lib/repositories/moeda_repository.dart
@@ -3,37 +3,37 @@ import '../model/moedaCripto.dart';
 class MoedasRepository {
   static List<MoedaCripto> tabela = [
     MoedaCripto(
-      icone: "cripto\images\png-transparent-ethereum-eth-hd-logo-thumbnail.png",
+      icone: "images/ethereum.png" ,
       nome: "Ethereum",
       preco: 9855.99,
       sigla: "ETC",
     ),
     MoedaCripto(
-      icone: "cripto\images\png-transparent-bitcoin-btc-coin-cryptocurrency-currency-and-cryptocurrency-signs-free-icon.png",
+      icone: "images/bitcoin.png",
       nome: "Bitcoin",
       preco: 1644603.00,
       sigla: "BTC",
     ),
     MoedaCripto(
-      icone: "cripto\images\cardano-ada-logo-4B6BADDB43-seeklogo.com.png",
+      icone: "images/cardano.png",
       nome: "Cardano",
       preco: 113.00,
       sigla: "ADA",
     ),
     MoedaCripto(
-      icone: "cripto\images\coin-flat-design-icon-png-favpng-XVc7Ban0jqBjkcStJfNJGbr9j.jpg",
+      icone: "images/usdcoin.png",
       nome: "USD Coin",
       preco: 53.00,
       sigla: "USDC",
     ),
     MoedaCripto(
-      icone: "cripto\images\127-1279698_ripple-coin-xrp-png-transparent-png.png",
+      icone: "images/xrp.png",
       nome: "XRP",
       preco: 123.00,
       sigla: "XRP",
     ),
     MoedaCripto(
-      icone: "cripto\images\Litecoin-Crypto-Logo-PNG.png",
+      icone: "images/litecoin.png",
       nome: "Litecoin",
       preco: 678.00,
       sigla: "LTC",
